8561a6030c0f008b73c4e0a55047dede410b0c4e
folkens2 <tfolkens@chicagobooth.edu>
2017-04-26 20:57:28 -0500

    the whole class

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,4 +11,35 @@ class PhotosController < ApplicationController
     render("photos/show.html.erb")
   end
 
+  def new_form
+    render("photos/new_form.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:id])
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    p = Photo.find(params[:id])
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos/#{p.id}")
+  end
+
+  def destroy_row
+    p = Photo.find(params[:id])
+    p.destroy
+    redirect_to("/photos")
+  end
+
 end
